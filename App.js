2bf776c82fd6589e9bc5950124b227cf9c320201
hakynbayo <akinbayo.akinwande@decagon.dev>
2023-03-05 00:33:38 +0100

    btn-component

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View,ImageBackground } from 'react-native';
 import CarItem from './components/carItem';
+import StyledBtn from './components/styledBtn';
 
 export default function App() {
   return (
     <View style={styles.container}>
      <CarItem/>
+     <StyledBtn type='secondary' content={'Custom Order'} onPress={()=>{
+      console.warn('Custom order button pressed')
+     }}/>
 
       <StatusBar style="auto" />
     </View>
